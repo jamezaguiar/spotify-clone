fe6b9769fd08944a2e0e815d29adf25694c76e0b
jamezaguiar <jamersonalsilva14@gmail.com>
2024-08-03 15:23:31 -0300

    footer finished

--- dummy-data/albums.ts
+++ dummy-data/albums.ts
@@ -6,6 +6,7 @@ export const DUMMY_ALBUMS = [
       "https://upload.wikimedia.org/wikipedia/pt/3/3b/Dark_Side_of_the_Moon.png",
     description:
       "Dark Side of the Moon é o oitavo álbum de estúdio da banda britânica Pink Floyd, lançado em 1 de março de 1973. É um dos álbuns mais aclamados pela crítica, e foi responsável por catapultar a banda a um sucesso mundial, tornando-se um dos álbuns mais vendidos de todos os tempos.",
+    artist: "Pink Floyd",
   },
   {
     title: "Nevermind",
@@ -13,6 +14,7 @@ export const DUMMY_ALBUMS = [
     cover: "https://upload.wikimedia.org/wikipedia/pt/3/3f/Nevermind.jpg",
     description:
       "Nevermind é o segundo álbum de estúdio da banda norte-americana Nirvana, lançado em 24 de setembro de 1991. Produzido por Butch Vig, foi o primeiro álbum lançado pela DGC Records, tendo sido seu lançamento mundial pela Geffen Records. Nevermind foi o segundo álbum de uma banda de rock alternativo a alcançar o primeiro lugar na Billboard 200, sucedendo o álbum Blood Sugar Sex Magik, da banda Red Hot Chili Peppers.",
+    artist: "Nirvana",
   },
   {
     title: "Led Zeppelin IV",
@@ -20,6 +22,7 @@ export const DUMMY_ALBUMS = [
     cover: "https://upload.wikimedia.org/wikipedia/pt/4/4b/LedZeppelinIV.jpg",
     description:
       'Led Zeppelin IV é o quarto álbum de estúdio da banda britânica de rock Led Zeppelin, lançado em 8 de novembro de 1971 pela Atlantic Records. O álbum foi produzido por Jimmy Page e gravado entre dezembro de 1970 e março de 1971 no Headley Grange, uma antiga casa rural no condado de Hampshire, Inglaterra. O álbum apresenta muitas das canções mais conhecidas do Led Zeppelin, incluindo "Black Dog", "Rock and Roll", "Going to California" e "Stairway to Heaven".',
+    artist: "Led Zeppelin",
   },
   {
     title: "A Night at the Opera",
@@ -28,12 +31,14 @@ export const DUMMY_ALBUMS = [
       "https://upload.wikimedia.org/wikipedia/pt/b/bf/A_Night_at_the_Opera_Queen.jpg",
     description:
       "A Night at the Opera é o quarto álbum de estúdio da banda britânica de rock Queen, lançado em 21 de novembro de 1975. Produzido por Roy Thomas Baker e Queen, foi o álbum mais caro já produzido na época. A Night at the Opera foi gravado no Rockfield Studios, em Monmouthshire, e no Sarm West e Wessex Studios, em Londres, entre agosto de 1975 e novembro de 1975.",
+    artist: "Queen",
   },
   {
     title: "Back in Black",
     href: "/playlist/back-in-black",
     cover: "https://upload.wikimedia.org/wikipedia/pt/b/b6/Back_in_Black.jpg",
     description: `Back in Black é o sétimo álbum de estúdio da banda australiana de rock AC/DC, lançado em 25 de julho de 1980. É o primeiro álbum a apresentar o vocalista Brian Johnson, que substituiu Bon Scott após sua morte em fevereiro de 1980. O álbum foi produzido por Robert John "Mutt" Lange e gravado no Compass Point Studios, em Nassau, Bahamas.`,
+    artist: "AC/DC",
   },
   {
     title: "Purple Rain",
@@ -42,5 +47,6 @@ export const DUMMY_ALBUMS = [
       "https://upload.wikimedia.org/wikipedia/pt/thumb/0/08/Purple-rain-capa.jpg/220px-Purple-rain-capa.jpg",
     description:
       "Purple Rain é o sexto álbum de estúdio do músico norte-americano Prince, lançado em 25 de junho de 1984 pela Warner Bros. Records. É o primeiro álbum a apresentar sua banda de apoio, The Revolution. Purple Rain foi o álbum mais vendido de 1984 e foi certificado como disco de diamante pela Recording Industry Association of America (RIAA).",
+    artist: "Prince",
   },
 ];
